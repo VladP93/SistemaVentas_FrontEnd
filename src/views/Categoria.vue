71878a19602b281a8e4b7ea29513596736c5e4c8
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2021-02-21 03:05:24 -0600

    Axios agregado, CRUD Categorías terminado

--- src/views/Categoria.vue
+++ src/views/Categoria.vue
@@ -16,7 +16,7 @@
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on }"
-            ><v-btn v-on="on" color="primary" dark class="mb-2">New Item</v-btn>
+            ><v-btn v-on="on" color="primary" dark class="mb-2">Nuevo</v-btn>
           </template>
           <v-card>
             <v-card-title>
@@ -26,35 +26,25 @@
             <v-card-text>
               <v-container grid-list-md>
                 <v-layout wrap>
-                  <v-flex xs12 sm6 md4>
+                  <v-flex xs12 sm12 md12>
                     <v-text-field
-                      v-model="editedItem.name"
-                      label="Dessert name"
+                      v-model="nombre"
+                      label="Nombre de categoría"
                     ></v-text-field>
                   </v-flex>
-                  <v-flex xs12 sm6 md4>
+                  <v-flex xs12 sm12 md12>
                     <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
+                      v-model="descripcion"
+                      label="Descripción"
                     ></v-text-field>
                   </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
+                  <v-flex xs12 sm12 md12 v-show="valida">
+                    <div
+                      class="red--text"
+                      v-for="v in validaMensaje"
+                      v-bind:key="v"
+                      v-text="v"
+                    ></div>
                   </v-flex>
                 </v-layout>
               </v-container>
@@ -63,10 +53,46 @@
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" text @click.native="close"
-                >Cancel</v-btn
+                >Cancelar</v-btn
               >
-              <v-btn color="blue darken-1" text @click.native="save"
-                >Save</v-btn
+              <v-btn color="blue darken-1" text @click.native="guardar"
+                >Guardar</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="adModal" max-width="350px">
+          <v-card>
+            <v-card-title class="headline" v-if="adAccion == 1"
+              >¿Activar Categoría?</v-card-title
+            >
+            <v-card-title class="headline" v-if="adAccion == 2"
+              >¿Desactivar Categoría?</v-card-title
+            >
+            <v-card-text>
+              Estás por
+              <span class="text--green" v-if="adAccion == 1">Activar</span>
+              <span class="text--red" v-if="adAccion == 2">Desactivar</span>
+              la categoría {{ adNombre }}
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="cerrarModalActivo"
+                >Cancelar</v-btn
+              >
+              <v-btn
+                v-if="adAccion == 1"
+                color="green darken-1"
+                text
+                @click="activar"
+                >Aceptar</v-btn
+              >
+              <v-btn
+                v-if="adAccion == 2"
+                color="green darken-1"
+                text
+                @click="desactivar"
+                >Aceptar</v-btn
               >
             </v-card-actions>
           </v-card>
@@ -74,67 +100,69 @@
       </v-toolbar>
       <v-data-table
         :headers="headers"
-        :items="desserts"
+        :items="categorias"
         :search="search"
         class="elevation-1"
       >
+        <template v-slot:[`item.condicion`]="{ item }">
+          <div v-if="item.condicion">
+            <span class="blue--text">Activo</span>
+          </div>
+          <div v-else>
+            <span class="red--text">Desactivado</span>
+          </div>
+        </template>
         <template v-slot:[`item.actions`]="{ item }">
           <v-icon small class="mr-2" @click="editItem(item)">
             edit
           </v-icon>
-          <v-icon small @click="deleteItem(item)">
-            delete
-          </v-icon>
+          <template v-if="item.condicion">
+            <v-icon small @click="activo(2, item)">
+              block
+            </v-icon>
+          </template>
+          <template v-else>
+            <v-icon small @click="activo(1, item)">
+              check
+            </v-icon>
+          </template>
         </template>
         <template v-slot:no-data>
-          <v-btn color="primary" @click="initialize">
-            Reset
-          </v-btn>
+          <span>No hay datos</span>
         </template>
       </v-data-table>
     </v-flex>
   </v-layout>
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
+      categorias: [],
       dialog: false,
       headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "left",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Actions", value: "actions", sortable: false },
+        { text: "Nombre", value: "nombre" },
+        { text: "Descripcion", value: "descripcion", sortable: false },
+        { text: "Estado", value: "condicion", sortable: false },
+        { text: "Opciones", value: "actions", sortable: false },
       ],
       search: "",
-      desserts: [],
       editedIndex: -1,
-      editedItem: {
-        name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
-      defaultItem: {
-        name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
+      id: "",
+      nombre: "",
+      descripcion: "",
+      valida: 0,
+      validaMensaje: [],
+      adModal: 0,
+      adAccion: 0,
+      adNombre: "",
+      adId: "",
     };
   },
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+      return this.editedIndex === -1 ? "Nueva categoría" : "Editando Categoría";
     },
   },
 
@@ -145,111 +173,142 @@ export default {
   },
 
   created() {
-    this.initialize();
+    this.listar();
   },
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-        },
-      ];
+    listar() {
+      let me = this;
+      axios
+        .get(`api/categorias/Listar`)
+        .then(function(response) {
+          me.categorias = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
 
     editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
+      this.idcategoria = item.idcategoria;
+      this.nombre = item.nombre;
+      this.descripcion = item.descripcion;
+      this.editedIndex = 1;
       this.dialog = true;
     },
 
-    deleteItem(item) {
-      const index = this.desserts.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.desserts.splice(index, 1);
+    activo(accion, item) {
+      this.adModal = 1;
+      this.adNombre = item.nombre;
+      this.adId = item.idcategoria;
+
+      if (accion === 1) {
+        this.adAccion = 1;
+      } else if (accion === 2) {
+        this.adAccion = 2;
+      } else {
+        this.adModal = 0;
+      }
+    },
+
+    activar() {
+      let me = this;
+      axios
+        .put(`api/Categorias/Activar/${me.adId}`, {})
+        .then(function(response) {
+          me.adModal = 0;
+          me.adAccion = 0;
+          me.adNombre = "";
+          me.adId = "";
+          me.listar();
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    },
+    desactivar() {
+      let me = this;
+      axios
+        .put(`api/Categorias/Desactivar/${me.adId}`, {})
+        .then(function(response) {
+          me.adModal = 0;
+          me.adAccion = 0;
+          me.adNombre = "";
+          me.adId = "";
+          me.listar();
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    },
+
+    cerrarModalActivo() {
+      this.adModal = 0;
     },
 
     close() {
       this.dialog = false;
-      setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      }, 300);
     },
 
-    save() {
+    limpiar() {
+      this.id = "";
+      this.nombre = "";
+      this.descripcion = "";
+      this.Index = -1;
+    },
+
+    guardar() {
+      if (this.validar()) {
+        return;
+      }
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+        //Editar
+        let me = this;
+        axios
+          .put("api/Categorias/Actualizar", {
+            idcategoria: me.idcategoria,
+            nombre: me.nombre,
+            descripcion: me.descripcion,
+          })
+          .then(function(res) {
+            me.close();
+            me.listar();
+            me.limpiar();
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
       } else {
-        this.desserts.push(this.editedItem);
+        let me = this;
+        axios
+          .post("api/categorias/Crear", {
+            nombre: me.nombre,
+            descripcion: me.descripcion,
+          })
+          .then(function(res) {
+            me.close();
+            me.listar();
+            me.limpiar();
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
       }
       this.close();
+    },
+
+    validar() {
+      this.valida = 0;
+      this.validaMensaje = [];
+      if (this.nombre.length < 3 || this.nombre.length > 50) {
+        this.validaMensaje.push(
+          "La longitud del nombre debe de estar entre 3 a 50 caracteres"
+        );
+      }
+      if (this.validaMensaje.length) {
+        this.valida = 1;
+      }
+
+      return this.valida;
     },
   },
 };
